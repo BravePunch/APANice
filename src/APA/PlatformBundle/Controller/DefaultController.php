4f487efab6e48d01b99254b3473bff71abec177d
BPunch <al.viguier@laposte.net>
2017-02-14 16:54:31 +0100

    securite

--- src/APA/PlatformBundle/Controller/DefaultController.php
+++ src/APA/PlatformBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace APA\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller
 {
@@ -13,6 +15,10 @@ class DefaultController extends Controller
 
     public function adminIndexAction()
     {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')){
+            throw new AccessDeniedException('Accès refusé.');
+        }
+
         return $this->render('APAPlatformBundle:Default:adminIndex.html.twig');
     }
 }
