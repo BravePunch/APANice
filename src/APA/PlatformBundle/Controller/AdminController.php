235765179f8d703a0ab19bb8743d86c70950542e
BPunch <al.viguier@laposte.net>
2017-02-20 13:58:55 +0100

    indentation, nom de dossier

--- src/APA/PlatformBundle/Controller/AdminController.php
+++ src/APA/PlatformBundle/Controller/AdminController.php
@@ -22,7 +22,7 @@ class AdminController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('APAPlatformBundle:Default:index.html.twig');
+        return $this->render('APAPlatformBundle:Admin:index.html.twig');
     }
 
     public function dynamicSearchAction(Request $request)
